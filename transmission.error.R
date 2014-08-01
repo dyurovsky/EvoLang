afd6da8c9feee2f5e52d1a35e4abcf5b98aa7a7d
Language and Cognition Lab <langcoglab-macbook@dn0a2229c2.sunet>
2014-07-31 17:11:34 -0700

    added transmission error script

---
+++ transmission.error.R
@@ -0,0 +1,19 @@
+#Normalized Levenshtein distance function
+normLev.fnc <- function(a, b) {
+  drop(adist(a, b) / nchar(attr(adist(a, b, counts = TRUE), "trafos")))
+}
+
+#Read in data. Row 1 = input data. Row 2 = participant 1. 
+data <- read.table("turk/words.csv",sep=",",header=TRUE, row.names=1, 
+                   stringsAsFactors=FALSE)
+
+#Segment out just the participants's responses. 
+responses <- data[,2:28]
+
+#Assign participants to a list for comparison. 
+L1 <- responses[10,]
+L2 <- responses[11,]
+
+#Calculate the normLD between the two sets of responses. 
+normLD <- diag(normLev.fnc(L1,L2))
+mean(normLD[])
